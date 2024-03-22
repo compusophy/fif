d775f8a79d37a4019562cb060c4fffa8dd74f00f
compusophy <79760496+compusophy@users.noreply.github.com>
2024-03-22 13:16:08 -0600

    display input text in next frame

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 import { Button, Frog, TextInput } from 'frog'
-import { neynar } from 'frog/hubs'
+//import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 
 const apiKeyNeynar = process.env.NEYNAR_API_KEY || ''; // Provide a default value for apiKeyNeynar
@@ -13,36 +13,16 @@ const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
   // Supply a Hub to enable frame verification.
-  hub: neynar({ apiKey: apiKeyNeynar })
+  //hub: neynar({ apiKey: apiKeyNeynar })
 })
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
 app.frame('/', async (c) => {
-  const { buttonValue, status, verified, frameData } = c
+  const { buttonValue, status, verified, frameData, inputText } = c
   
-  if (status === 'response') {
-    try {
-      // Make an external API call to https://api.neynar.com/v2/farcaster/user/follow
-      const response = await fetch('https://api.neynar.com/v2/farcaster/user/follow', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          api_key: apiKeyNeynar, // Replace this with your actual API key
-        },
-        body: JSON.stringify({
-          signer_uuid: signerUUID, // Replace this with your actual signer UUID
-          target_fids: [frameData!.fid], // Assuming fid is a string
-        }),
-      });
-
-      const data = await response.json();
-      // Handle the response data here
-    } catch (error) {
-      // Handle any errors here
-    }
-  }
+  let displayMessage = status === 'response' ? inputText : 'FRAME IN FRAME';
 
   return c.res({
     image: (
@@ -70,19 +50,14 @@ app.frame('/', async (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `success!!`
-            : 'FRAME IN FRAME'}
+          {displayMessage}
         </div>
       </div>
     ),
     intents: [
-      // Only show the initial button if status is not 'response'
-      status !== 'response' && <TextInput placeholder="ENTER FRAME URL..." />,
-      status !== 'response' && <Button value="frameRender">RENDER</Button>,
-      // Only show the Reset button if status is 'response'
-      status === 'response' && <Button.Reset>reset</Button.Reset>,
-    ].filter(Boolean),
+      <TextInput placeholder="ENTER FRAME URL..." />,
+      <Button value="frameRender">RENDER</Button>,
+    ]
   })
 })
 
